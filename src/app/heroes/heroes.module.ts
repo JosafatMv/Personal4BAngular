4d436cd0195204f0cebb0f10b5d73efe1ce8c6c0
JosafatMV <20213tn124@utez.edu.mx>
2022-11-09 09:59:08 -0600

    2

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AddHeroeComponent } from './add/add.component';
 import { ListComponent } from './list/list.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ListComponent, AddHeroeComponent],
-  imports: [],
+  imports: [CommonModule],
   providers: [],
   exports: [ListComponent, AddHeroeComponent]
 })
